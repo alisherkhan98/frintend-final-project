54a444e81b7c95ece0fc842f75f59852aa5fd340
Alisher laptop <asherkhan982@gmail.com>
2022-11-24 17:35:57 +0100

    prettier

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,24 @@
+// react
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
+// MUI
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Button,
+  Typography,
+  Toolbar,
+} from "@mui/material";
+// icons
 import { FaBars } from "react-icons/fa";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 
+// constants
 const drawerWidth = "66vw";
 const navItems = ["Home", "About", "Contact"];
 
